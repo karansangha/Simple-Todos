95ac6a429ca92fd5fa9995dbe760db40f1ec8729
Karan Sangha <iamkaransangha@gmail.com>
2016-12-30 00:28:44 -0800

    add a form to enter tasks from the webpage
    and store it in the database

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -12,9 +12,25 @@ class TodosListCtrl {
 
         this.helpers({
             tasks() {
-                return Tasks.find({});
+                // Show newest tasks at the top
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
             }
         })
+    }
+
+    addTask(newTask) {
+        // Insert a task into the collection
+        Tasks.insert({
+            text: newTask,
+            createdAt: new Date
+        });
+
+        // Clear form
+        this.newTask = '';
     }
 }
 
